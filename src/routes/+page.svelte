9e999a59226b083f6687001db011f86ed55a35c7
Gabriel Mack <gabrielmack210@gmail.com>
2023-06-02 22:26:07 -0700

    awesome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let text = '';
+	const regex = /\S/;
+	let tasks = [];
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		if (regex.test(text)) {
+			tasks = [...tasks, { task: text }];
+			text = '';
+			console.log(tasks);
+		}
+	}
+
+	function handleDelete(index) {
+		tasks = tasks.filter((_, i) => i !== index);
+	}
+</script>
+
+<body class="flex flex-col h-screen bg-slate-100 items-center justify-center">
+	<h1 class="mx-auto mb-[3rem]">Enter the task</h1>
+
+	<form class="flex mb-[1rem]" on:submit={handleSubmit}>
+		<input type="text" bind:value={text} />
+		<button type="submit">Submit</button>
+	</form>
+
+	<ul class="text-center h-[12.5rem] overflow-y-scroll">
+		{#each tasks as task, index}
+			<li>
+				<span>{task.task}</span>
+				<button on:click={() => handleDelete(index)}>🗑️</button>
+			</li>
+		{/each}
+	</ul>
+</body>
